fcbe5810d7efd7174396f7e6cd56fc3ae389ad32
vremyavnikuda <andrew.nevsky@yandex.ru>
2026-02-25 21:24:04 +0700

    upd(sys): config
    - hyprland
    - wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,7 +4,19 @@ local config = wezterm.config_builder()
 
 config.enable_wayland = true
 config.term = 'wezterm'
-config.font_size = 11.0
+config.font = wezterm.font 'Cascadia Code'
+config.font_size = 12.0
+config.harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' }
+config.font_rules = {
+  {
+    italic = true,
+    font = wezterm.font { family = 'Cascadia Code', italic = true },
+  },
+  {
+    intensity = 'Bold',
+    font = wezterm.font { family = 'Cascadia Code', bold = true },
+  },
+}
 config.initial_cols = 120
 config.initial_rows = 30
 config.color_scheme = 'Kanagawa (Gogh)'
@@ -13,4 +25,3 @@ config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 
 return config
-
